5668b0e93b803ae266ba5a082192fdae01d342e9
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-09-29 02:15:24 +0900

    chore: enable jupyterlab

--- tasks.py
+++ tasks.py
@@ -2,12 +2,12 @@ from invoke import task
 import os
 
 JUPYTER_CONFIG = f"{os.getcwd()}/.jupyter/jupyter_notebook_config.py"
-JUPYTER_NOTEBOOK_DIR = f"{os.getcwd()}/notebook"
+JUPYTER_NOTEBOOK_DIR = f"{os.getcwd()}"
 
 @task
 def jupyter(c):
     c.run(f"""
-      jupyter notebook                        \\
+      jupyter lab                             \\
         --config {JUPYTER_CONFIG}             \\
         --notebook-dir {JUPYTER_NOTEBOOK_DIR}
     """)
